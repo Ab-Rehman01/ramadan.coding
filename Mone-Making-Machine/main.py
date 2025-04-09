4d4d4495096247eb6e72ab00966bb6bdeb0b3e6c
Ab-Rehman01 <161899946+Ab-Rehman01@users.noreply.github.com>
2025-04-09 17:24:39 +0500

    main

--- Mone-Making-Machine/main.py
+++ Mone-Making-Machine/main.py
@@ -5,15 +5,31 @@ import requests
 
 st.title("Money Making Machine")
 def generate_money():
-    return random.randint(1, 100)
+    return random.randint(1, 1000)
 st.subheader("inatNT Cash generator")
 
 if st.button("Generate Money"):
     st.write("counting money...")
-    time.sleep(1)
+    time.sleep(5)
     amount = generate_money()
     st.success(f"You have earned ${generate_money()} inatNT cash!")
-
+def fetch_side_hustle():
+    try:
+        response = requests.get("http://127.0.0.1:8000/side-hustle")
+        if response.status_code == 200:
+            hustles = response.json()
+            return hustles
+        else:
+            return []
+    except Exception as e:
+        st.error(f"Error fetching side hustles: {e}")
+        return []
+    else:
+        return ("something went wrong!")
+st.subheader("side hustle ideas") 
+if st.button("Generate hustle"):
+      idea=fetch_side_hustle()
+      st.success(idea)
 
 
 
